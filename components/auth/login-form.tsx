2b956446c917cf276ecb17df23c877d50625ce70
assilbekov <askhat.assilbekov@gmail.com>
2024-03-21 15:45:55 -0300

    LoginForm email added

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,22 @@
+"use client"
+
+import * as z from "zod"
+import { useForm } from "react-hook-form"
 import { CardWrapper } from "./card-wrapper"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="Welcome back!"
@@ -8,7 +24,35 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login form!
+      <Form {...form}>
+        <form 
+        className="space-y-6"
+        onSubmit={form.handleSubmit(() => {})}
+        >
+          <div className="space-y-4">
+            <FormField 
+            name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      className="w-full"
+                      placeholder="john.doe@example.com"
+                    />
+                  </FormControl>
+                  <FormMessage>
+                    {form.formState.errors.email?.message}
+                  </FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   )
 }
