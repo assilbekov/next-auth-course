16d98f21542ac51fae756a2e405f994accc9c1e1
assilbekov <askhat.assilbekov@gmail.com>
2024-03-22 15:32:21 -0300

    getUserById added

--- data/user.ts
+++ data/user.ts
@@ -1,5 +1,19 @@
 import { db } from "@/lib/db";
 
 export const getUserByEmail = async (email: string) => {
-  return db.user.findUnique({ where: { email } });
+  try {
+    return await db.user.findUnique({ where: { email } });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export const getUserById = async (id: string) => {
+  try {
+    return await db.user.findUnique({ where: { id } });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
