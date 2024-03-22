e31d633552049b48a76f8c2e460cdae6d0928d7a
assilbekov <askhat.assilbekov@gmail.com>
2024-03-22 14:27:11 -0300

    register added

--- actions/login.ts
+++ actions/login.ts
@@ -1,14 +1,14 @@
 "use server"
 
 import * as z from "zod"
-import {LoginSchema} from "@/schemas"
- 
+import { LoginSchema } from "@/schemas"
+
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return {error: "Invalid fields!"};
+    return { error: "Invalid fields!" };
   }
 
-  return {message: "Email sent!"}
+  return { message: "Email sent!" }
 }
