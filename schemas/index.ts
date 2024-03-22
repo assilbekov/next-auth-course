c1a4228441fdd42f81d61c38c8edc00ae2988323
assilbekov <askhat.assilbekov@gmail.com>
2024-03-22 14:19:17 -0300

    RegisterSchema added

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,10 @@ export const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(1, { message: "Password is required"}),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({message: "Invalid email"}),
+  name: z.string().min(1, { message: "Name is required"}),
+  password: z.string().min(6, { message: "Password must be at least 6 characters long"}),
+  passwordConfirmation: z.string(),
+}).refine(data => data.password === data.passwordConfirmation, { message: "Passwords must match"});
