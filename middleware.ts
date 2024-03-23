6cfe34b5a70bf109bebd2d25120c856b0d459bf1
assilbekov <askhat.assilbekov@gmail.com>
2024-03-23 18:29:12 -0300

    auth middleware added

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { auth } from "./auth"
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  console.log("Middleware called: " + req.nextUrl.pathname)
+  console.log("User is logged in: " + isLoggedIn)
+})
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    // Re-include any files in the api or trpc folders that might have an extension
+    "/(api|trpc)(.*)"
+  ],
+}
