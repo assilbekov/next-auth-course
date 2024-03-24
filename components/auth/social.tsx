174edb42461dee8da0b258997c9cd31e055a6902
assilbekov <askhat.assilbekov@gmail.com>
2024-03-23 22:42:16 -0300

    social login buttons added

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,17 +3,21 @@
 import { FcGoogle } from "react-icons/fc"
 import { Button } from "../ui/button"
 import { FaGithub } from "react-icons/fa"
+import { signIn } from "next-auth/react"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT })
+  }
+
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {
-          console.log("Google")
-        }}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="w-5 h-5" />
       </Button>
@@ -21,9 +25,7 @@ export const Social = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {
-          console.log("Github")
-        }}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="w-5 h-5" />
       </Button>
