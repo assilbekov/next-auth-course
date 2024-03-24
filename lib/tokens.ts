4be41ec66c6fad68c04a5fb821468c18efb46ddc
assilbekov <askhat.assilbekov@gmail.com>
2024-03-24 16:11:43 -0300

    generateVerificationToken added

---
+++ lib/tokens.ts
@@ -0,0 +1,23 @@
+import { getVerificationTokenByEmail } from "@/data/verification-token"
+import { v4 as uuidv4 } from "uuid"
+import { db } from "./db"
+
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4()
+  const expires = new Date((new Date()).getTime() + 1000 * 60 * 60 * 24) // 24 hours from now
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await db.verificationToken.delete({
+      where: { id: existingToken.id },
+    })
+  }
+
+  const verificationToken = await db.verificationToken.create({
+    data: { email, token, expires }
+  })
+
+  return verificationToken
+}
