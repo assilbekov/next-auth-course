300bab159f29ad1618b1d76729e01c0008ec2e38
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 01:48:28 -0300

    use reset in Reset form

--- components/auth/reset-form.tsx
+++ components/auth/reset-form.tsx
@@ -4,32 +4,31 @@ import * as z from "zod"
 import { useForm } from "react-hook-form"
 import { CardWrapper } from "./card-wrapper"
 import { zodResolver } from "@hookform/resolvers/zod";
-import { LoginSchema } from "@/schemas";
+import { ResetSchema } from "@/schemas";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { useState, useTransition } from "react";
-import { login } from "@/actions/login";
+import { reset } from "@/actions/reset";
 
 export const ResetForm = () => {
   const [error, setError] = useState<string | undefined>("")
   const [success, setSuccess] = useState<string | undefined>("")
   const [isPending, startTransition] = useTransition()
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof ResetSchema>>({
+    resolver: zodResolver(ResetSchema),
     defaultValues: {
       email: "",
-      password: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
     setError("")
     setSuccess("")
     startTransition(() => {
-      login(values)
+      reset(values)
         .then((data) => {
           setError(data?.error)
           setSuccess(data?.success)
