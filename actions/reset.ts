51f86f2c6c9067db57e3a69dd0fcd3c3f0d5dece
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 01:59:27 -0300

    reset action fixed

--- actions/reset.ts
+++ actions/reset.ts
@@ -3,6 +3,8 @@
 import * as z from "zod"
 import { ResetSchema } from "@/schemas"
 import { getUserByEmail } from "@/data/user"
+import { generatePasswordResetToken } from "@/lib/tokens"
+import { sendPasswordResetEmail } from "@/lib/mail"
 
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
@@ -20,7 +22,8 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
     return { error: "Email doesn't exist!" };
   }
 
-  // TODO: generate token and send email
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(passwordResetToken.email, passwordResetToken.token);
 
   return { success: "Reset email sent!" };
 }
