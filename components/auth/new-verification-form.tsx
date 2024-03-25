8d3d1040578bc3e35061da5c83f4e94d0efa0f51
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 00:37:13 -0300

    handle newVerification in NewVerificationForm

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -3,14 +3,31 @@
 import { BeatLoader } from "react-spinners"
 import { CardWrapper } from "./card-wrapper"
 import { useSearchParams } from "next/navigation"
-import { useCallback, useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { newVerification } from "@/actions/new-verification"
+import { FormError } from "../form-error"
+import { FormSuccess } from "../form-success"
 
 export const NewVerificationForm = () => {
+  const [error, setError] = useState("")
+  const [success, setSuccess] = useState("")
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
 
   const onSubmit = useCallback(() => {
-    //
+    if (!token) {
+      setError("Token not found")
+      return
+    }
+
+    newVerification(token)
+      .then((response) => {
+        setError(response.error || "")
+        setSuccess(response.success || "")
+      })
+      .catch(() => {
+        setError("An error occurred")
+      })
   }, [token])
 
   useEffect(() => {
@@ -24,9 +41,11 @@ export const NewVerificationForm = () => {
       backButtonHref="/auth/login"
       showSocial={false}
     >
-      <p className="flex items-center w-full justify-center">
-        <BeatLoader />
-      </p>
+      <div className="flex items-center w-full justify-center">
+        {!error && !success && <BeatLoader />}
+        <FormError message={error} />
+        <FormSuccess message={success} />
+      </div>
     </CardWrapper>
   )
 }
