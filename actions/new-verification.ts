8f8807b08f19f3316abee0c4dd7a305ae9a57a5d
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 00:30:56 -0300

    newVerification added success message

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -27,4 +27,10 @@ export const newVerification = async (token: string) => {
     where: { email: existingToken.email },
     data: { emailVerified: new Date(), email: existingToken.email },
   })
+
+  await db.verificationToken.delete({
+    where: { id: existingToken.id },
+  })
+
+  return { success: "Email verified successfully" }
 }
