7e69ad8e8bfcaf18738159e308ac56a457336c2d
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 02:19:00 -0300

    NewPasswordSchema added

--- schemas/index.ts
+++ schemas/index.ts
@@ -18,3 +18,11 @@ export const RegisterSchema = z.object({
 export const ResetSchema = z.object({
   email: z.string().email(),
 });
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+  passwordConfirmation: z.string(),
+}).refine(
+  data => data.password === data.passwordConfirmation,
+  { message: "Passwords must match", path: ["passwordConfirmation"] }
+);
