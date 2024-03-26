b741a44b8bcd75106d43c4271964237e78b56a19
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 22:11:56 -0300

    Navbar content started

--- app/(protected)/settings/_components/navbar.tsx
+++ app/(protected)/settings/_components/navbar.tsx
@@ -1,9 +1,20 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
 export const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    <nav className="bg-white p-4 shadow-md">
-      <h1>Navbar</h1>
+    <nav className="bg-secondary flex items-center justify-between p-4 rounded-xl w-[600px] shadow-sm">
+      <div className="flex   gap-x-2">
+        <Button asChild variant="link">
+          <Link href="/settings">Settings</Link>
+        </Button>
+      </div>
+      <p>user button</p>
     </nav>
   )
 }
