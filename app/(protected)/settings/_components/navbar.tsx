204ff9b3b911f8bd4ee7698d517a039a934a68a6
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 22:16:32 -0300

    ButtonLink added

--- app/(protected)/settings/_components/navbar.tsx
+++ app/(protected)/settings/_components/navbar.tsx
@@ -4,15 +4,30 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
+interface ButtonLinkProps {
+  children: React.ReactNode
+  href: string
+}
+
+const ButtonLink = ({ href, children }: ButtonLinkProps) => {
+  const pathname = usePathname();
+  return (
+    <Button asChild variant={pathname === href ? "default" : "link"}>
+      <Link href={href}>{children}</Link>
+    </Button>
+  )
+}
+
 export const Navbar = () => {
   const pathname = usePathname();
 
   return (
     <nav className="bg-secondary flex items-center justify-between p-4 rounded-xl w-[600px] shadow-sm">
-      <div className="flex   gap-x-2">
-        <Button asChild variant="link">
-          <Link href="/settings">Settings</Link>
-        </Button>
+      <div className="flex gap-x-2">
+        <ButtonLink href="/settings">Settings</ButtonLink>
+        <ButtonLink href="/server">Server</ButtonLink>
+        <ButtonLink href="/client">Client</ButtonLink>
+        <ButtonLink href="/admin">Admin</ButtonLink>
       </div>
       <p>user button</p>
     </nav>
