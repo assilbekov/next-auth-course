04345513c2610c2bc9c24489bdce0b533bc06e40
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 22:27:24 -0300

    UserButton styling added

--- components/auth/user-button.tsx
+++ components/auth/user-button.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
 import { useCurrentUser } from "@/hooks"
 import { LogoutButton } from "./logout-button"
+import { ExitIcon } from "@radix-ui/react-icons"
 
 export const UserButton = () => {
   const user = useCurrentUser();
@@ -16,9 +17,10 @@ export const UserButton = () => {
           <AvatarFallback className="bg-sky-500"><FaUser className="text-white" /></AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent className="w-40" align="end">
         <LogoutButton>
           <DropdownMenuItem>
+            <ExitIcon className="w-4 h-4 mr-2" />
             Sign out
           </DropdownMenuItem>
         </LogoutButton>
