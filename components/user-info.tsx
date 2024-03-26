2df4103bc1c83b574b9db5a1effe76c06b82e3e5
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 23:11:51 -0300

    content render for UserInfo

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -22,11 +22,24 @@ export const UserInfo = ({ user, label }: UserInfoProps) => {
         </p>
       </CardHeader>
       <CardContent>
-        <p className="text-lg font-semibold">{user.name}</p>
-        <p className="text-sm text-gray-500">{user.email}</p>
-        <p className="text-sm text-gray-500">{user.role}</p>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">ID</p>
+          <p className="truncate text-sm max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">{user.id}</p>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Name</p>
+          <p className="truncate text-sm max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">{user.name}</p>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Role</p>
+          <p className="truncate text-sm max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">{user.role}</p>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Email</p>
+          <p className="truncate text-sm max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">{user.email}</p>
+        </div>
+        <LogoutButton>Sign out</LogoutButton>
       </CardContent>
-      <LogoutButton>Sign out</LogoutButton>
     </Card>
   )
 }
