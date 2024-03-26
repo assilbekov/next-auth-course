19278ad3e40aca8f7d6ca05ef2f0731d9b1466c7
assilbekov <askhat.assilbekov@gmail.com>
2024-03-25 21:59:14 -0300

    SettingsPage now is a client compoennt

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,20 @@
-import { auth, signOut } from "@/auth"
+"use client"
+
 import { Button } from "@/components/ui/button";
+import { useSession, signOut } from "next-auth/react";
 
+export default function SettingsPage() {
+  const session = useSession();
 
-export default async function SettingsPage() {
-  const session = await auth();
+  const onClick = () => {
+    signOut();
+  }
+
   return (
     <div>
       <h1>Settings page</h1>
-      <p>
-        {JSON.stringify(session)}
-      </p>
-      <form action={async () => {
-        "use server"
-
-        await signOut();
-      }}>
-        <Button type="submit">Sign out</Button>
-      </form>
+      <p>{JSON.stringify(session)}</p>
+      <Button onClick={onClick}>Sign out</Button>
     </div>
   )
 }
